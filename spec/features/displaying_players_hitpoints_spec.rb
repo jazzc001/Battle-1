290a5324ab280f85529b9b5f6fb34cc69c09a94c
jazzc001 <jazzcheung212@gmail.com>
2022-03-10 14:37:58 +0000

    DRY up feature test
    Co-authored-by: lorn888 <lorn888@no-reply.github.com>

--- spec/features/displaying_players_hitpoints_spec.rb
+++ spec/features/displaying_players_hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature "player hitpoints" do
   scenario "checking players hitpoints" do
-      visit('/')
-      fill_in(name: 'player_1', with: 'Jasmine')
-      fill_in(name: 'player_2', with: 'Patryk')
-      click_button('submit')
+      sign_in_and_play
       expect(page).to have_content "Patryk's hitpoints: 100hp"
   end
 end
--- spec/features/submitting_form_spec.rb
+++ spec/features/submitting_form_spec.rb
@@ -1,9 +1,6 @@
 feature "entering names" do
     scenario "players enter names" do
-        visit('/')
-        fill_in('player_1', with: 'Jasmine')
-        fill_in('player_2', with: 'Patryk')
-        click_button('submit')
+        sign_in_and_play
         expect(page).to have_content 'Hello Jasmine and Patryk!'
       end
 end
